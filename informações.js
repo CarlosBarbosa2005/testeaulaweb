31332d75298cbf9a3a892a9e3f2cb9ef1b2f64fa
CarlosBarbosa2005 <91471783+CarlosBarbosa2005@users.noreply.github.com>
2021-10-17 17:38:39 -0300

    Update informações.js

--- informações.js
+++ informações.js
@@ -1,3 +1,9 @@
 <script>
-  
+  const topicos = nem Array();
+  lista = document.querySelector('ol');
+  topicos.forEach(topico=>{
+      const item = document.createElement('li')
+      item.textcontent = topico;
+      lista.append(item);
+  });
   </script>
