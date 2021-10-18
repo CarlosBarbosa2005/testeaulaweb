523920a0559343b39694e61ced836457bb2b85ba
CarlosBarbosa2005 <91471783+CarlosBarbosa2005@users.noreply.github.com>
2021-10-18 15:58:39 -0300

    Update conteudo.js

--- conteudo.js
+++ conteudo.js
@@ -37,10 +37,27 @@ item.append(link);
 
 document.querySelector("body > center > ol > li:nth-child(1)").innerHTML = "<a href='#flamengo'>flamengo</a>";
 document.querySelector("body > center > ol > li:nth-child(2)").innerHTML = "<a href='#corinthians'>corinthians</a>";
-document.querySelector("body > center > ol > li:nth-child(3)").innerHTML = "<a href='#sao-paulo'>sao-paulo</a>";
+document.querySelector("body > center > ol > li:nth-child(3)").innerHTML = "<a href='#saopaulo'>saopaulo</a>";
 document.querySelector("body > center > ol > li:nth-child(4)").innerHTML = "<a href='#palmeiras'>palmeiras</a>";
 document.querySelector("body > center > ol > li:nth-child(5)").innerHTML = "<a href='#vasco'>vasco</a>";
 document.querySelector("body > center > ol > li:nth-child(6)").innerHTML = "<a href='#cruzeiro'>cruzeiro</a>";
 
+var flamengo = document.querySelector("#flamengo");
+flamengo.insertAdjacentHTML('afterend', "<a href='#'>ÍNICIO</a><br>");
+
+var cruzeiro = document.querySelector("#cruzeiro");
+cruzeiro.insertAdjacentHTML('afterend', "<a href='#'>ÍNICIO</a><br>");
+
+var saopaulo = document.querySelector("#saopaulo");
+saopaulo.insertAdjacentHTML('afterend', "<a href='#'>ÍNICIO</a><br>");
+
+var vasco = document.querySelector("#vasco");
+vasco.insertAdjacentHTML('afterend', "<a href='#'>ÍNICIO</a><br>");
+
+var corinthians = document.querySelector("#corinthians");
+corinthians.insertAdjacentHTML('afterend', "<a href='#'>ÍNICIO</a><br>");
+
+var palmeiras = document.querySelector("#palmeiras");
+palmeiras.insertAdjacentHTML('afterend', "<a href='#'>ÍNICIO</a><br>");
 
  
