e8d8674b02934abb24e307b994035c27dc759d02
CarlosBarbosa2005 <91471783+CarlosBarbosa2005@users.noreply.github.com>
2021-10-18 15:05:30 -0300

    Update conteudo.js

--- conteudo.js
+++ conteudo.js
@@ -1,35 +1,38 @@
 const subtitulos = document.querySelectorAll ('h2');
 const topicos = new Array();
 subtitulos.forEach(element => {
+//insere todos os textos em topicos 	
     topicos.push(element.textContent);
+	
+/* cria e insere a ancora para cada seção 	
     const ancora = document.createElement('a');
     ancora.setAttribute('name', element.textContent);
     element.append(ancora);
+	
+//cria e insere o link para retornar ao início	
     const retorno = document.createElement('a');
     retorno.setAttribute('href','#');
     retorno.textContent = "início";
     element.parentElement.insertBefore(retorno, element.nextElementSibling);
+    */
  });	
 	  
  const lista = document.querySelector('ol');
   topicos.forEach(topico=> {
+	  
+//insere os elementos da lista 	  
       const item = document.createElement('li')
       item.textContent = topico;
       lista.append(item)
+	  
+	/*  
+//criar e insere o link de cada seção	  
 const link = document.createElement('a');
 link.setAttribute('href',`#${topico}`);
 link.textContent = topico;
 item.append(link);
+*/
 
   });
-  
-$('.voltar a[href^="#"]').on('click', function(e) {
-	e.preventDefault();
-	var id = $(this).attr('href'),
-			targetOffset = $(id).offset().top;
-			
-	$('html, body').animate({ 
-		scrollTop: targetOffset - 100
-	}, 500);
-});
+
  
